a5d4254b019b7aa3aec279be6fc0064bc68a6e8a
user <santoshmishra6663@gmail.com>
2023-01-11 10:21:44 +0545

    polyline first

--- lib/google_map.dart
+++ lib/google_map.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
+
 const LatLng currentLocation = LatLng(27.7172, 85.3240);
 
 class Google_map extends StatefulWidget {
@@ -18,20 +19,36 @@ class _Google_mapState extends State<Google_map> {
         .onError((error, stackTrace) {
       print("error" + error.toString());
     });
-    return await Geolocator.getCurrentPosition();
+    Position position =await Geolocator.getCurrentPosition();
+    return position;
   }
 
-  final Completer<GoogleMapController> _controller = Completer();
+  late List<LatLng> LatLen=[
+
+    LatLng(27.676435938958118, 85.31298520024238),
+    LatLng(27.699987553306677, 85.29008913886423),
+    LatLng(27.700899476916465, 85.29129076851122),
+    LatLng(27.681899494282664, 85.30880022908163),
+  ];
+  late GoogleMapController googleMapController;
+  final Set<Polyline> _polyline = {};
+
   BitmapDescriptor markerIcon = BitmapDescriptor.defaultMarker;
 
   void initState() {
     addCustomIcon();
     super.initState();
+    _polyline.add(
+      Polyline(polylineId:PolylineId ('polyline1'),
+        color: Colors.red,
+        points:LatLen,
+      )
+    );
   }
 
   void addCustomIcon() {
     BitmapDescriptor.fromAssetImage(
-      ImageConfiguration(devicePixelRatio: 0.01),
+      ImageConfiguration(),
       "assets/Logo1.png",
     ).then(
       (icon) {
@@ -43,29 +60,25 @@ class _Google_mapState extends State<Google_map> {
   }
 
   final List<Marker> _markers = <Marker>[
-    Marker(markerId: MarkerId('1'),
-    position: LatLng(27.676435938958118, 85.31298520024238),
-      infoWindow: InfoWindow(
-        title:'Nepal Red Cross Society Lalitpur Blood Bank'
-      )
+    Marker(
+        markerId: MarkerId('1'),
+        position: LatLng(27.676435938958118, 85.31298520024238),
+        infoWindow:
+            InfoWindow(title: 'Nepal Red Cross Society Lalitpur Blood Bank')),
+    Marker(
+      markerId: MarkerId('2'),
+      position: LatLng(27.699987553306677, 85.29008913886423),
+      infoWindow:
+          InfoWindow(title: 'नेपाल रेडक्रस सोसाईटी रक्त बैंक, सोल्टीमोड'),
     ),
-    Marker(markerId: MarkerId('2'),
-    position: LatLng(27.699987553306677, 85.29008913886423),
-      infoWindow: InfoWindow(
-        title:'नेपाल रेडक्रस सोसाईटी रक्त बैंक, सोल्टीमोड'
-      )
-    ),
-    Marker(markerId: MarkerId('3'),
-    position: LatLng(27.700899476916465, 85.29129076851122),
-      infoWindow: InfoWindow(
-        title:'central blood bank Rabibhawan'
-      )
-    ),
-    Marker(markerId: MarkerId('4'),
-    position: LatLng(27.681899494282664, 85.30880022908163),
-      infoWindow: InfoWindow(
-        title:'Hamro LifeBank'
-      )
+    Marker(
+        markerId: MarkerId('3'),
+        position: LatLng(27.700899476916465, 85.29129076851122),
+        infoWindow: InfoWindow(title: 'central blood bank Rabibhawan')),
+    Marker(
+      markerId: MarkerId('4'),
+      position: LatLng(27.681899494282664, 85.30880022908163),
+      infoWindow: InfoWindow(title: 'Hamro LifeBank'),
     ),
   ];
 
@@ -76,37 +89,29 @@ class _Google_mapState extends State<Google_map> {
         initialCameraPosition: CameraPosition(
           target: currentLocation,
           zoom: 12,
-
         ),
         markers: Set<Marker>.of(_markers),
-
+        polylines: _polyline,
         onMapCreated: (GoogleMapController controller) {
-          _controller.complete(controller);
+          googleMapController=controller;
         },
-
-
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          getUserCurrentLocation().then((value) async {
-            print(value.latitude.toString() + " " + value.longitude.toString());
+        onPressed: () async {
+          Position position = await  getUserCurrentLocation();
+          googleMapController.animateCamera(CameraUpdate.newCameraPosition(CameraPosition(target: LatLng(position.latitude,position.longitude),zoom: 14)));
+
             _markers.add(Marker(
                 markerId: MarkerId('0'),
-                position: LatLng(value.latitude, value.longitude),
+                position: LatLng(position.latitude, position.longitude),
                 infoWindow: InfoWindow(title: 'Current Location'),
                 icon: markerIcon));
 
-            CameraPosition cameraPosition = CameraPosition(
-                zoom: 14, target: LatLng(value.latitude, value.longitude));
-            final GoogleMapController controller = await _controller.future;
-            controller
-                .animateCamera(CameraUpdate.newCameraPosition(cameraPosition));
             setState(() {});
-          });
-        },
-        child: Icon(Icons.location_on),
-      ),
 
+        },
+        child: Icon(Icons.my_location_outlined,),
+      ),
     );
   }
 }
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,10 +5,12 @@
 import FlutterMacOS
 import Foundation
 
+import flutter_local_notifications
 import geolocator_apple
 import package_info_plus
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  FlutterLocalNotificationsPlugin.register(with: registry.registrar(forPlugin: "FlutterLocalNotificationsPlugin"))
   GeolocatorPlugin.register(with: registry.registrar(forPlugin: "GeolocatorPlugin"))
   FLTPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FLTPackageInfoPlusPlugin"))
 }
